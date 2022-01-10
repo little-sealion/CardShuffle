d651689ea76da563d1e75ca2ee3bb407bc658394
Tess Zheng <tess.zheng@outlook.com>
2022-01-11 08:29:15 +1000

    Add files via upload

---
+++ Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace CardShuffle
+{
+	public static class Extensions
+	{
+		//You can see the addition of the this modifier on the first argument to the method. That means you call the method
+        //as though it were a member method of the type of the first argument.
+		public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first,IEnumerable<T> second)
+        {
+			var firstIter = first.GetEnumerator();
+			var secondIter = second.GetEnumerator();
+			while(firstIter.MoveNext() && secondIter.MoveNext())
+            {
+				yield return firstIter.Current;
+				yield return secondIter.Current;
+			}
+        }
+
+		public static bool SequenceEquals<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+			var firstIter = first.GetEnumerator();
+			var secondIter = second.GetEnumerator();
+			while(firstIter.MoveNext() && secondIter.MoveNext())
+            {
+				if (!firstIter.Current.Equals( secondIter.Current)) return false;
+            }
+			return true;
+        }
+
+		public static IEnumerable<T> LogQuery<T>(this IEnumerable<T> sequence, string tag)
+        {
+			// File.AppendText creates a new file if the file doesn't exist.
+			using (var writer = File.AppendText("debug.log"))
+			{
+				writer.WriteLine($"executing query {tag}");
+			}
+			return sequence;
+		}
+
+	}
+}
+
